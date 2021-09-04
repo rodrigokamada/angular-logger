3bbe51c26c686132f05d2b0f01cd8e8439f028e7
Rodrigo Kamada <rodrigo@kamada.com.br>
2021-09-04 17:03:39 -0300

    Fixing the test.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientModule } from '@angular/common/http';
+import { LoggerModule } from 'ngx-logger';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
@@ -9,6 +10,7 @@ describe('AppComponent', () => {
       imports: [
         RouterTestingModule,
         HttpClientModule,
+        LoggerModule.forRoot(null),
       ],
       declarations: [
         AppComponent,
