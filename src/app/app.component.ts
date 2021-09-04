df3c9a1d17dbfa3013c1680a2f7eab5682e344bf
Rodrigo Kamada <rodrigo@kamada.com.br>
2021-09-04 16:30:35 -0300

    Adding the log level methods.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,29 @@ import { NGXLogger } from 'ngx-logger';
 export class AppComponent {
 
   constructor(private logger: NGXLogger) {
+  }
+
+  public showTrace(): void {
     this.logger.trace('TRACE level');
+  }
+
+  public showDebug(): void {
     this.logger.debug('DEBUG level');
+  }
+
+  public showInfo(): void {
     this.logger.info('INFO level');
+  }
+
+  public showWarn(): void {
     this.logger.warn('WARN level');
+  }
+
+  public showError(): void {
     this.logger.error('ERROR level');
+  }
+
+  public showFatal(): void {
     this.logger.fatal('FATAL level');
   }
 
